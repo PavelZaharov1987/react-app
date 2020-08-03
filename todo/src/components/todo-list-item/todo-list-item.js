c51f55578b0e9adeec8cb6f902286b7c5415cdd8
PavelZakharov <pavelzakharov1987@gmail.com>
2020-08-04 01:09:28 +0300

    feat: add func for delete, add and change component

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -3,29 +3,70 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-    render() {
-        const { label, important = false } = this.props;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'steelblue' : 'black',
+    // constructor() {
+    //     super();
+
+    //     this.onLabelClick = () => {
+    //         console.log(`Done: ${this.props.label}`);
+    //     }
+
+    //     this.state = {
+    //         done: false
+    //     }
+    // }
+
+    // state = {
+    //     done: false,
+    //     important: false
+    // } больше нам state не нужен
+
+    onLabelClick = () => {
+        this.setState(({done}) => {
+            return {
+                done: !done
+            }
+        });
+    }
+
+    onMarkImportant = () => {
+        this.setState((state) => { //можно деструктурировать, как в done!!!
+            return {
+                important: !state.important
+            };
+        });
+    }
+
+    render() {
+        const { label, onDeleted, onToggleImportant, onToggleDone, done, important } = this.props;
+        // const { done, important } = this.state; когда мы подняли их на уровень App, они стали приходить к нам из props
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
         }
+
+       if (important) {
+           classNames += ' important';
+       }
     
         return (
-            <span className="todo-list-item">
+            <span className={ classNames }>
                 <span 
                     className="todo-list-item-label"
-                    style={style}>
+                    onClick={ onToggleDone }>
                     { /*props.label*/ label }
                 </span>
     
                 <button type="button"
-                        className="btn btn-outline-success btn-sm float-right">
+                        className="btn btn-outline-success btn-sm float-right"
+                        onClick={ onToggleImportant }>
                     <i className="fa fa-exclamation" />
                 </button>
     
                 <button type="button"
-                        className="btn btn-outline-danger btn-sm float-right">
+                        className="btn btn-outline-danger btn-sm float-right"
+                        onClick={onDeleted}>
                     <i className="fa fa-trash-o" />
                 </button>
             </span>
--- todo/src/components/todo-list/todo-list.js
+++ todo/src/components/todo-list/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item'
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
 
     const elements = todos.map((item) => {
 
@@ -14,7 +14,10 @@ const TodoList = ({ todos }) => {
                 <TodoListItem 
                     /*label={item.label}
                     important={item.important}*/
-                    { ...itemProps } />
+                    { ...itemProps }
+                    onDeleted={() => onDeleted(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleDone={() => onToggleDone(id)} />
             </li>
         );
     })
